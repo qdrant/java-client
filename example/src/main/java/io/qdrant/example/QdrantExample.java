95f4e5540aea4e16a10b1f9c6f7cdb4a1702d4cf
Anush008 <anushshetty90@gmail.com>
2025-08-19 10:29:45 +0530

    chore: Simplified client instantiation
    Signed-off-by: Anush008 <anushshetty90@gmail.com>

--- example/src/main/java/io/qdrant/example/QdrantExample.java
+++ example/src/main/java/io/qdrant/example/QdrantExample.java
@@ -1,39 +1,30 @@
 package io.qdrant.example;
 
-import io.grpc.Grpc;
-import io.grpc.InsecureChannelCredentials;
-import io.grpc.ManagedChannel;
 import io.qdrant.client.QdrantClient;
 import io.qdrant.client.QdrantGrpcClient;
-import io.qdrant.client.grpc.Collections;
-import io.qdrant.client.grpc.Collections.CreateCollection;
 import io.qdrant.client.grpc.Collections.VectorParams;
 import io.qdrant.client.grpc.Collections.Distance;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 
 public class QdrantExample {
     public static void main(String[] args) {        
         try {
-            ManagedChannel channel =
-                Grpc.newChannelBuilder(
-                        "localhost:6334", InsecureChannelCredentials.create())
-                    .build();
-            QdrantGrpcClient grpcClient = QdrantGrpcClient.newBuilder(channel, true).build();
-            QdrantClient client = new QdrantClient(grpcClient);
+            QdrantClient client = new QdrantClient(QdrantGrpcClient.newBuilder("localhost", 6334, false)
+            .build());
                         
-            
+                        
             // Create a simple collection
             String collectionName = "example_collection";
-            
-            client.createCollectionAsync(
-                    collectionName,
-                    VectorParams.newBuilder()
-                        .setDistance(Distance.Cosine)
-                        .setSize(128)
-                        .build()
-                ).get();
-            
+
+            if (!client.collectionExistsAsync(collectionName).get()) {
+                client.createCollectionAsync(
+                        collectionName,
+                        VectorParams.newBuilder()
+                            .setDistance(Distance.Cosine)
+                            .setSize(128)
+                            .build()
+                    ).get();
+            }
 
             List<String> collections = client.listCollectionsAsync().get();
             
